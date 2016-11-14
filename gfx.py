48e6dfb64ce2c71a5522976e9fb67d14bff28298
Crocodeele <maxkovalovs@gmail.com>
2016-11-14 21:34:43 +0000

    gfx test made

--- gfx.py
+++ gfx.py
@@ -20,10 +20,24 @@ class Planet():
 
         pygame.draw.circle(game_window.surface,(255,255,255),(planet_x,planet_y),3)
 
+    def move_to(self,game_window,pos_x,pos_y):
+        pygame.draw.circle(game_window.surface, (0,0,0), (self.x, self.y), 3)
+        pygame.draw.circle(game_window.surface, (255, 255, 255), (pos_x, pos_y), 3)
+
+        self.x = pos_x
+        self.y = pos_y
+
 #opened for testing
 if __name__=='__main__':
     pygame.init()
     window = Game_Window(500,500)
     mars = Planet(window,100,50,50)
     window.update()
+    fps_clock = pygame.time.Clock()
+    fps_limit = 60
 
+    for coord in range(50,400):
+        mars.move_to(window,coord,coord)
+        window.update()
+
+        fps_clock.tick(fps_limit)
