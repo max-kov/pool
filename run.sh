43483c696054bc68225adbfd5e9428c504ad4e6b
Max Kovalovs <maxkovalovs@gmail.com>
2017-08-14 11:25:28 +0100

    Reworked the run script.

--- run.sh
+++ run.sh
@@ -1,3 +1,28 @@
 #!/bin/bash
-source pool/bin/activate
-python3.5 pool/main.py
+if !(command -v python3 > /dev/null 2>&1;) then
+    echo python3 was not found on your system
+    echo please check README.md for instructions
+else
+    if !(python3 -c "import venv" &> /dev/null;) then
+        echo no venv module was found in python3
+        echo please check README.md for instructions
+    fi
+    if !(python3 -c "import pip" &> /dev/null;) then
+        echo no pip module was found in python3
+        echo please check README.md for instructions
+    fi
+    if (python3 -c "import venv" &> /dev/null;) &&
+    (python3 -c "import pip" &> /dev/null;) then
+        if [ -e pool/bin/activate ]; then
+            source pool/bin/activate
+        else
+            python3 -m venv pool
+            source pool/bin/activate
+            echo installing venv packages
+            python3 -m pip install -r requirements.txt
+            echo Succesfully installed a virtual environment.
+        fi
+        python3 pool/main.py
+        deactivate
+    fi
+fi
